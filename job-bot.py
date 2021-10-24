db8e0f696e436fc5b2044f88581b7afa9e4bf346
Ghost <chloeurisohn@gmail.com>
2021-10-24 16:29:50 +0100

    first commit

--- job-bot.py
+++ job-bot.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 driver = webdriver.Chrome(".\chromedriver.exe")
 driver.get("https://www.linkedin.com/")
 
 signin_button = driver.find_element_by_class_name("nav__button-secondary")
 signin_button.click()
+username = driver.find_element_by_id("username")
+username.send_keys(os.getenv('EMAIL'))
