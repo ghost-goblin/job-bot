6a8151826ad27795c03f7cf54d48c4e6d40a2222
Ghost <chloeurisohn@gmail.com>
2021-10-24 16:39:45 +0100

    first commit

--- job-bot.py
+++ job-bot.py
@@ -11,4 +11,10 @@ driver.get("https://www.linkedin.com/")
 signin_button = driver.find_element_by_class_name("nav__button-secondary")
 signin_button.click()
 username = driver.find_element_by_id("username")
-username.send_keys(os.getenv('EMAIL'))
+username.send_keys(os.getenv("EMAIL"))
+password = driver.find_element_by_id("password")
+password.send_keys(os.getenv("PASSWORD"))
+
+
+main_signin_button = driver.find_element_by_class_name("btn__primary--large")
+main_signin_button.click()
